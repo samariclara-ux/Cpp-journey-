b0607699a969558abd8dae4ac1f9942615f3ee92
samariclara-ux <samariclarance@gmail.com>
2026-03-19 05:36:07 +0100

    arrays
    learning arrays in cpp ,understanding it functions and how to make it do what i want

---
+++ claa.cpp
@@ -0,0 +1,15 @@
+#include  <iostream>
+int main() {
+
+  // Arrays
+
+  double prices[5] = { 19.99, 29.99, 39.99, 49.99, 59.99 };
+  std::cout << prices[0] << '\n';
+  std::cout << prices[1] << '\n';
+  std::cout << prices[2] << '\n';
+  std::cout << prices[3] << '\n';
+  std::cout << prices[4] << '\n';
+
+  return 0;
+}
+
